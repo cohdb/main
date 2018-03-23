2af3284d41647d74c7c26d846abdab1d35ead206
ryantaylor <ryan.taylor9@gmail.com>
2018-03-22 20:40:32 -0400

    Player select component.

---
+++ src/app/forms/select/player-select/PlayerSelect.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { List } from 'immutable';
+import Select from 'react-select';
+
+import './PlayerSelect.css';
+
+const getOptionsForPlayers = players => new List(players.map(player => ({ value: player.id, label: player.name }))).toArray();
+
+class PlayerSelect extends React.PureComponent {
+  handleChange = option => this.props.onChange(option.value);
+
+  render = () => (
+    <Select
+      options={getOptionsForPlayers(this.props.players)}
+      onChange={this.handleChange}
+      value={this.props.value}
+      style={{ width: 250 }}
+      clearable={false}
+      deleteRemoves={false}
+    />
+  )
+}
+
+export default PlayerSelect;
