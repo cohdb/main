0cd5e9bd3c10846c75352b04f0cadaf974f1a628
ryantaylor <ryan.taylor9@gmail.com>
2018-03-22 20:39:38 -0400

    Replay download section.

---
+++ src/app/replays/replay-utilities/ReplayUtilities.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+
+import Wrapper from '../../wrapper/Wrapper';
+import Placeholder from '../../placeholder/Placeholder';
+import { Link } from '../../routing/links/Links';
+import { isFulfilled } from '../../../utils/statusHelpers';
+
+import './ReplayUtilities.css';
+
+const LoadingState = () => (
+  <Row>
+    <Col xs={12}>
+      <span><Placeholder>Download</Placeholder></span>
+    </Col>
+  </Row>
+);
+
+const Content = ({ replay }) => (
+  <Row>
+    <Col xs={12}>
+      <Link
+        to={replay.url}
+        text="Download"
+        external
+      />
+    </Col>
+  </Row>
+);
+
+const ReplayUtilities = ({ replay, status }) => (
+  <Wrapper paddingTop={35}>
+    <div className="dbReplayUtilities-card dbReplayUtilities-cardSmall dbReplayUtilities">
+      {isFulfilled(status) && <Content replay={replay} />}
+      {!isFulfilled(status) && <LoadingState />}
+    </div>
+  </Wrapper>
+);
+
+export default ReplayUtilities;
