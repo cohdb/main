e1abc22e3be2093d8e59b9c0620f69202aef5145
ryantaylor <ryan.taylor9@gmail.com>
2018-03-22 20:38:49 -0400

    Pass children to SubHeader and use flex to right-align.

--- src/app/sub-header/SubHeader.jsx
+++ src/app/sub-header/SubHeader.jsx
@@ -6,7 +6,7 @@ const BLUE_CODE = '#0EBFE9';
 const RED_CODE = '#FF4500';
 const DEFAULT_CODE = '#FFF';
 
-const SubHeader = ({ title, blue = false, red = false, shadow = false }) => {
+const SubHeader = ({ children, title, blue = false, red = false, shadow = false }) => {
   let color;
   if (blue) {
     color = BLUE_CODE;
@@ -19,11 +19,12 @@ const SubHeader = ({ title, blue = false, red = false, shadow = false }) => {
   return (
     <div className="dbSubHeader">
       <h2
-        className={shadow ? 'dbSubHeader-shadow' : undefined}
+        className={`dbSubHeader-title ${shadow && 'dbSubHeader-shadow'}`}
         style={{ color }}
       >
         {title.toUpperCase()}
       </h2>
+      {children && <div className="dbSubHeader-content">{children}</div>}
     </div>
   );
 };
