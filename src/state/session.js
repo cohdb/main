1eb94dd9678a02182b72e4bdf8483f2ebac795dc
ryantaylor <ryan.taylor9@gmail.com>
2018-03-23 19:15:11 -0400

    Update session to handle auth.

--- src/state/session.js
+++ src/state/session.js
@@ -1,14 +1,41 @@
 import { createSelector } from 'reselect';
+import axios from 'axios';
+
 import { getUserEntities } from './users';
 
 const INITIAL_STATE = {
   id: null,
+  accessToken: null
 };
 
-const session = (state = INITIAL_STATE) => state;
+const FETCH_ACCESS_TOKEN = 'session/accessToken/FETCH';
+export const FETCH_ACCESS_TOKEN_PENDING = 'session/accessToken/FETCH_PENDING';
+export const FETCH_ACCESS_TOKEN_REJECTED = 'session/accessToken/FETCH_REJECTED';
+export const FETCH_ACCESS_TOKEN_FULFILLED = 'session/accessToken/FETCH_FULFILLED';
+
+const session = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    case FETCH_ACCESS_TOKEN_FULFILLED:
+      return { ...state, accessToken: extractTokenFromPayload(payload) };
+
+    default:
+      return state;
+  }
+};
 
 export default session;
 
+const AUTH_URL = '/auth';
+
+export const extractTokenFromPayload = payload => payload.data.data.attributes.token;
+
+export const fetchAccessToken = token => ({
+  type: FETCH_ACCESS_TOKEN,
+  payload: axios.post(AUTH_URL, { login_token: token })
+});
+
+// SELECTORS
+//
 export const sessionSelector = createSelector(
   state => state.session,
   session => session
