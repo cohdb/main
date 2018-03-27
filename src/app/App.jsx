8d773893f98efa0fa1cb579d9946db260a25c8b3
ryantaylor <ryan.taylor9@gmail.com>
2018-03-27 00:31:27 -0400

    Auth overlay and loading separation.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider, connect } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import Overlay from './overlay/Overlay';
 import Layout from './layout/Layout';
 import NotFound from './not-found/NotFound';
 import Home from './home/Home';
@@ -12,23 +13,36 @@ import { fetchMyUser } from '../state/users';
 
 import './App.css';
 
+const Routes = () => (
+  <Router>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/auth" component={Auth} />
+        <Route exact path="/replays/:id" component={ReplayContainer} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
+  </Router>
+);
+
 class App extends React.Component {
-  componentWillMount = () =>
+  state = {
+    authFinished: false
+  };
+
+  componentWillMount = () => {
     this.props.dispatch(loadAccessTokenFromStorage())
-      .then(() => this.props.dispatch(fetchMyUser()));
+      .then(() => this.props.dispatch(fetchMyUser()))
+      .finally(() => this.setState({ authFinished: true }));
+  };
 
   render = () => (
     <Provider store={this.props.store}>
-      <Router>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/auth" component={Auth} />
-            <Route exact path="/replays/:id" component={ReplayContainer} />
-            <Route component={NotFound} />
-          </Switch>
-        </Layout>
-      </Router>
+      <React.Fragment>
+        {!this.state.authFinished && <Overlay />}
+        {this.state.authFinished && <Routes />}
+      </React.Fragment>
     </Provider>
   );
 }
