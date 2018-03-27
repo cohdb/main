9da9dda97c84ff2e41437cec0fbbcb81b59050b1
ryantaylor <ryan.taylor9@gmail.com>
2018-03-27 00:33:40 -0400

    Update auth middleware.

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,12 @@ import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
 
-import updateAuth from './updateAuth';
+import auth from './auth';
 import reducers from '../state';
 
 export default function (props, devEnv) {
   const composedStore = compose(
-    applyMiddleware(...[promise(), thunk, updateAuth, devEnv && createLogger()].filter(Boolean)),
+    applyMiddleware(...[promise(), thunk, auth, devEnv && createLogger()].filter(Boolean)),
     devEnv && window.devToolsExtension ? window.devToolsExtension() : f => f,
   );
   const storeCreator = composedStore(createStore);
