f8d504810158751f29859dc0f1c394e41b8ddd0b
ryantaylor <ryan.taylor9@gmail.com>
2018-10-08 01:18:18 -0400

    Load current user with apollo/GQL.

--- src/app/nav/Nav.jsx
+++ src/app/nav/Nav.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Query } from 'react-apollo';
 
 import Wrapper from '../wrapper/Wrapper';
 import { Link } from '../routing/links/Links';
 import { currentUserSelector } from '../../state/session';
+import { AUTH_USER } from '../../graphql/queries/users';
 
 import './Nav.css';
 
-const SiteNav = ({ currentUser }) => (
-  <header>
-    <Wrapper light>
-      <Navbar className="dbNav" expand="xl">
-        <NavbarBrand className="dbNav-brand" tag="span"><Link to="/" text="cohdb" /></NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            {!currentUser.id && <NavLink className="dbNav-link" tag="span"><Link to="/auth" text="Sign In with Steam" fa="steam" /></NavLink>}
-            {currentUser.id && <NavLink className="dbNav-link" tag="span"><Link to="/" text={`${currentUser.name} (${currentUser.nickname})`} /></NavLink>}
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </Wrapper>
-  </header>
+const SiteNav = () => (
+  <Query query={AUTH_USER}>
+    {({ data: currentUser }) => (
+      <header>
+        <Wrapper light>
+          <Navbar className="dbNav" expand="xl">
+            <NavbarBrand className="dbNav-brand" tag="span"><Link to="/" text="cohdb" /></NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                {!currentUser.id && <NavLink className="dbNav-link" tag="span"><Link to="/auth" text="Sign In with Steam" fa="steam" /></NavLink>}
+                {currentUser.id && <NavLink className="dbNav-link" tag="span"><Link to="/" text={`${currentUser.name} (${currentUser.nickname})`} /></NavLink>}
+              </NavItem>
+            </Nav>
+          </Navbar>
+        </Wrapper>
+      </header>
+    )}
+  </Query>
 );
 
-const mapStateToProps = state => currentUserSelector(state);
-
-export default connect(mapStateToProps)(SiteNav);
+export default SiteNav;
